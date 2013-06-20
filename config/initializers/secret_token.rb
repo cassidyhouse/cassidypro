0436bd2db84b47b805dc0dda17a7276d1b6c02d4
mmehrotra <mudit.mehrotra@triffort.com>
2013-06-20 08:56:08 +0530

    product page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CassidyProduct::Application.config.secret_token = '23d3a0b67545c28ab09ca8439d42c44b51956f34fd399b39547bd22603fcff8091bc0ebeebefacae79aa524d941543499708dcedcf17229bb1d2092026d74848'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+CassidyProduct::Application.config.session_store :cookie_store, key: '_CassidyProduct_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# CassidyProduct::Application.config.session_store :active_record_store
